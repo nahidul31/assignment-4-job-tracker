3c97a711f17a3633ff6784149d4f590adab05dfc
nahidul31 <nahidulislam2383@email.com>
2026-02-23 13:41:00 +0600

    add interview and rejected section

--- srcipts/machine.js
+++ srcipts/machine.js
@@ -3,3 +3,14 @@ function getValueById(id) {
   // console.log(value);
   return value;
 }
+
+function btnop(id) {
+  const interview = getValueById("interview-container");
+  const rejected = getValueById("reajected-container");
+  const alljobInfo = getValueById("show-all-jobs");
+  const selected = getValueById(id);
+  interview.classList.add("hidden");
+  rejected.classList.add("hidden");
+  alljobInfo.classList.add("hidden");
+  selected.classList.remove("hidden");
+}
